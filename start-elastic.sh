72bc2d2e05b819337ab7f394a65df8a46de99101
Jegan Vincent <jegan@abcs-MacBook-Pro-4.local>
2020-01-08 11:44:20 +0530

    fixed ownership

--- start-elastic.sh
+++ start-elastic.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 sudo sysctl -p /etc/sysctl.d/99-z01-sysctl.conf
 
+echo "Fixing daa dir ownership"
+
+sudo chown -R elastic:elastic /home/elastic/data
+
 su - elastic /home/elastic/bin/elasticsearch &
 
 /bin/bash -c "trap : TERM INT; sleep infinity & wait"
